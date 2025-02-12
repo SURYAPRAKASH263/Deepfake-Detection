8702519a71b6da79f9100dbad3d9076d47921bf5
SURYAPRAKASH263 <twenty6surya@gmail.com>
2025-02-13 02:17:22 +0530

    updating files

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask
+import os
+import socket
+from waitress import serve
 
 app = Flask(__name__)
 
 # ...existing code...
 
+def find_open_port():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("", 0))
+    s.listen(1)
+    port = s.getsockname()[1]
+    s.close()
+    return port
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    PORT = int(os.environ.get("PORT", find_open_port()))  # Find an open port if PORT is not set
+    serve(app, host="0.0.0.0", port=PORT)
