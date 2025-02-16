97559211bc547d46d8bd4309c1dfff6e9ea25235
SURYAPRAKASH263 <twenty6surya@gmail.com>
2025-02-16 19:52:33 +0530

    Updated build.sh and other changes

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
+#!/bin/bash
 set -o errexit
-
 pip install -r requirements.txt
-
 python manage.py migrate
-if [[ $CREATE_SUPERUSER ]]; then
+
+if [ "$CREATE_SUPERUSER" = "true" ]; then
     python manage.py createsuperuser --noinput --email "$DJANGO_SUPERUSER_EMAIL"
 fi
 python manage.py runserver
